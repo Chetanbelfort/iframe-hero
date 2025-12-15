2e7bbee5ceeb218c5582f8e241c632ab7eab5d4b
Chetan Sharma <chetanbelfort@gmail.com>
2025-12-16 03:05:20 +0530

    iframe hero working

---
+++ app/hero/page.tsx
@@ -0,0 +1,15 @@
+export default function Page() {
+  return (
+    <div style={{
+      height: '700px',
+      backgroundColor: 'black',
+      color: 'white',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '32px',
+    }}>
+      HERO IS LIVE
+    </div>
+  );
+}
