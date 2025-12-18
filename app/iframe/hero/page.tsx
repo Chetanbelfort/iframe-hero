fc88da046798eb85705ca0520b48d0ed5dcefdc5
Chetan Sharma <chetanbelfort@gmail.com>
2025-12-18 12:07:00 +0530

    update landing page sections

--- app/iframe/hero/page.tsx
+++ app/iframe/hero/page.tsx
@@ -1,10 +1,6 @@
-import HeroSection from '@/components/sections/HeroSection'
+import FloatingIconsHero from '@/components/ui/floating-icons-hero-section'
+import { icons } from '@/components/hero-icons' // or wherever icons are
 
-export default function Home() {
-  return (
-    <>
-      <HeroSection />
-      {/* other sections */}
-    </>
-  )
+export default function IframeHeroPage() {
+  return <FloatingIconsHero icons={icons} />
 }
